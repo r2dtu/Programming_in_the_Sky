2b9df6a1d90a15befa620b5952d3f11388ba25a3
David <dctu@ucsd.edu>
2018-04-09 21:06:59 -0700

    I guess things look different in VIM than IDLE

--- solution_code/ProgrammingInTheSky.py
+++ solution_code/ProgrammingInTheSky.py
@@ -1,19 +1,19 @@
 """
-File: 			TicTacToe.py
+File: 		TicTacToe.py
 Description: 	Part of the IEEExHKN Programming in the Sky Workshop on April
-				22, 2018. This file runs a Tic Tac Toe game, where a player
-				will play against an automated computer player.
-Editor:			<YOUR NAME>
+		22, 2018. This file runs a Tic Tac Toe game, where a player
+		will play against an automated computer player.
+Editor:		<YOUR NAME>
 """
 
 from random import *
 
 # the tic tac toe board
 board = [
-			["_","_","_"],
-			["_","_","_"],
-			["_","_","_"]
-		]
+		["_","_","_"],
+		["_","_","_"],
+		["_","_","_"]
+	]
 
 def printBoard():
 	"""
@@ -28,10 +28,10 @@ def printBoard():
 
 def isWon(player):
 	"""
-	Parameters:		player - x or o, indicating which character to check
+	Parameters:	player - x or o, indicating which character to check
 	Description:	Checks if the player has won the game. This is done by
-					checking each row, col and diagonal.
-	Returns:		True if the player wins the game, False otherwise.
+			checking each row, col and diagonal.
+	Returns:	True if the player wins the game, False otherwise.
 	"""
 	for i in range(3):
 		if board[i][0] == player and board[i][1] == player and board[i][2] == player:
@@ -59,15 +59,15 @@ def validMove(row, col):
 
 def humanTurn( symbol ):
 	"""
-	Parameters:		symbol - which character to display for the human
+	Parameters:	symbol - which character to display for the human
 	Description: 	Executes the human's turn. The command line will ask
-					you to input a row and a column. Then it will check whether 
-					the square has already been occupied. If yes, it will ask 
-					you again to input another row and col. If it is valid, it 
-					will place your piece onto the board and print it. It also 
-					checks whether you wins the game if you place your piece in 
-					that square.
-	Returns:		True if you win the game. False otherwise.
+			you to input a row and a column. Then it will check whether 
+			the square has already been occupied. If yes, it will ask 
+			you again to input another row and col. If it is valid, it 
+			will place your piece onto the board and print it. It also 
+			checks whether you wins the game if you place your piece in 
+			that square.
+	Returns:	True if you win the game. False otherwise.
 	"""
 	row, col = input("Enter your move: (row column):").split()
 	row = int(row)
@@ -90,14 +90,14 @@ def humanTurn( symbol ):
 
 def computerTurn( symbol ):
 	"""
-	Parameters:		symbol - which character to display for the human
+	Parameters:	symbol - which character to display for the human
 	Description:	Executes the computer's turn. It will generate a random 
-					move for the computer. Just like with the human move,
-					if the move is valid, it will place the piece onto the 
-					board and print it. It also checks whether computer wins 
-					the game .
+			move for the computer. Just like with the human move,
+			if the move is valid, it will place the piece onto the 
+			board and print it. It also checks whether computer wins 
+			the game.
 
-	Returns:		True if the computer wins the game. False otherwise.
+	Returns:	True if the computer wins the game. False otherwise.
 	"""
 	row = randint(0, 2)
 	col = randint(0, 2)
@@ -118,8 +118,8 @@ def computerTurn( symbol ):
 def humanFirst():
 	"""
 	Description:	Called if the human player goes first. Loop between human 
-					and computer turns and constantly check if the return
-					value from those is True, signifying that the game is over.
+			and computer turns and constantly check if the return
+			value from those is True, signifying that the game is over.
 	"""
 	for i in range(4):
 		if humanTurn("x") is True:
@@ -134,9 +134,9 @@ def humanFirst():
 def computerFirst():
 	"""
 	Description:	Called if the computer player goes first. Loop between 
-					human and computer turns and constantly check if the 
-					return value from those is True, signifying that the game 
-					is over.
+			human and computer turns and constantly check if the 
+			return value from those is True, signifying that the game 
+			is over.
 	"""
 	computerTurn("x")
 	for i in range(4):
@@ -151,9 +151,9 @@ def computerFirst():
 def main():
 	"""
 	Description:	Runs the main program. Determines whether the human or
-					computer player goes first by generating a random number 
-					between 0 and 1. If it is 0, then human goes first,
-					otherwise the computer player goes first.
+			computer player goes first by generating a random number 
+			between 0 and 1. If it is 0, then human goes first,
+			otherwise the computer player goes first.
 	"""
 	move = randint(0,1)
 	if move is 0:

ecdf671588a0fbc07452960a44a2dce1a38df10f
David <dctu@ucsd.edu>
2018-04-15 11:13:33 -0700

    added SVM exercise solutions

--- solution_code/03-SVM-Exercises.py
+++ solution_code/03-SVM-Exercises.py
@@ -14,7 +14,7 @@ plt.xlabel(iris.feature_names[1])
 plt.ylabel(iris.feature_names[3])
 plt.show()
 
-# Support Vector Classifier
+# Exercise 2 - Support Vector Classifier
 from sklearn.svm import SVC
 clf = SVC(kernel='linear', C=10)
 clf.fit(data, labels)
@@ -30,7 +30,7 @@ plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1])
 pf.plot_svc_decision_function(clf)
 plt.show()
 
-# Exercise 3
+# Exercise 3 - plot entire dataset
 data, labels = iris.data, iris.target
 data = data[:, (1, 3)]
 
@@ -40,17 +40,18 @@ plt.xlabel(iris.feature_names[1])
 plt.ylabel(iris.feature_names[3])
 plt.show()
 
-# Support Vector Classifier
+# Exercise 4 - Support Vector Classifier
 clf = SVC(kernel='linear')
 clf.fit(data, labels)
 
 # Plot SVC decision
+xx, yy = pf.make_meshgrid(data[:, 0], data[:, 1], h=500)
+pf.plot_contours(plt, clf, xx, yy, cmap='winter_r')
 plt.scatter(data[:, 0], data[:, 1], c=labels, cmap='autumn');
-pf.plot_svc_decision_function(clf)
 plt.show()
 
 # Plot support vectors
+pf.plot_contours(plt, clf, xx, yy, cmap='winter_r')
 plt.scatter(data[:, 0], data[:, 1], c=labels, cmap='autumn');
 plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1])
-pf.plot_svc_decision_function(clf)
 plt.show()

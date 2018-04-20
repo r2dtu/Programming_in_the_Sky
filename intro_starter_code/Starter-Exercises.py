0ec06679c2813c7b2dbb48b28d2c66c0ec17aac3
eminguyen <emilyhuongnguyen@gmail.com>
2018-04-20 04:21:04 -0700

    Add warmup exercises

---
+++ intro_starter_code/Starter-Exercises.py
@@ -0,0 +1,63 @@
+"""
+File: Starter-Exercises
+Description: Part of the IEEExHKN Programming in the Sky Workshop on April
+		     22, 2018. This file simply contains a few quick warmup exercises.
+Editor:	Antony Nguyen, Godwin Pang
+"""
+
+"""
+Comments
+-----
+These exercises will cover comments
+"""
+
+# TODO Comment this line out
+Comment me out
+
+"""
+Numbers
+-----
+These exercises will cover basic numbers
+"""
+
+# TODO Print any number
+
+# TODO Add any two numbers together
+
+"""
+Strings
+-----
+These exercises will cover strings
+"""
+
+# TODO Print Hello World
+
+# TODO Print the following sentence
+# Chickens can't moo
+
+# TODO Print the following sentence
+# The cow says "Moo" (NOTICE THE QUOTATION MARKS)
+
+"""
+Variables
+"""
+
+# TODO Print 'the sky' using the variables whats and up
+whats = "the "
+up = 'sky"
+
+# TODO Print 5 using the two variables
+two = "2"
+three = "3"
+
+# TODO Print "Hello World" using the variable hello
+hello = "Goodbye" # DO NOT DELETE OR CHANGE THIS LINE
+print(hello) # DO NOT DELETE OR CHANGE THIS LINE
+
+# TODO Print "My move is 0, 0" using row and col
+row = 1
+col = 2
+
+# TODO Print "My move is 0, 0" using row and col WITHOUT changing row and col
+row = 1
+col = 2

3124c958031fbaaf03f18c960cce2821c66d92e7
eminguyen <emilyhuongnguyen@gmail.com>
2018-04-21 08:58:58 -0700

    Fix indentation for logic exercises

--- intro_starter_code/Logic-Exercises.py
+++ intro_starter_code/Logic-Exercises.py
@@ -13,27 +13,27 @@ else:
     print("You failed Exercise 1")
 
 # TODO  Exercise 2
-    if (1+1 is ''''''): # Fill this line out
-        print("You passed Exercise 2")
-    else:
-        print("You failed Exercise 2")
+if (1+1 is ''''''): # Fill this line out
+    print("You passed Exercise 2")
+else:
+    print("You failed Exercise 2")
 
 # TODO  Exercise 3
-    if ("2"+"3" is ''''''): # Fill this line out
-        print("You passed Exercise 3")
-    else:
-        print("You failed Exercise 3")
+if ("2"+"3" is ''''''): # Fill this line out
+    print("You passed Exercise 3")
+else:
+    print("You failed Exercise 3")
 
 # TODO  Exercise 4
-    if ((True and False) is ''''''): # Fill this line out
-        print("You passed Exercise 4")
-    else:
-        print("You failed Exercise 4")
+if ((True and False) is ''''''): # Fill this line out
+    print("You passed Exercise 4")
+else:
+    print("You failed Exercise 4")
 
 # TODO  Exercise 5
-    if ((False or not False) is ''''''): # Fill this line out
-        print("You passed Exercise 5")
-    else:
-        print("You failed Exercise 5")
+if ((False or not False) is ''''''): # Fill this line out
+    print("You passed Exercise 5")
+else:
+    print("You failed Exercise 5")
 
 # TODO  Write your own if / else in TicTacToe-Exercises.py (validMove())

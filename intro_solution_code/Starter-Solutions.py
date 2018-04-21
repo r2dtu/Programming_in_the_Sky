1501f21e466448e71fa17783e6b6cef84fbffbdb
eminguyen <emilyhuongnguyen@gmail.com>
2018-04-21 08:57:23 -0700

    Add solution code to introduction exercises

---
+++ intro_solution_code/Starter-Solutions.py
@@ -0,0 +1,68 @@
+"""
+File: Starter-Exercises
+Description: Part of the IEEExHKN Programming in the Sky Workshop on April
+		     22, 2018. This file simply contains a few quick warmup exercises.
+Editor:	Antony Nguyen, Godwin Pang
+"""
+
+"""
+Comments
+-----
+These exercises will cover comments
+"""
+
+# TODO Comment this line out
+#Comment me out
+
+"""
+Numbers
+-----
+These exercises will cover basic numbers
+"""
+
+# TODO Print any number
+print(5)
+
+# TODO Add any two numbers together
+print(5+5)
+
+"""
+Strings
+-----
+These exercises will cover strings
+"""
+
+# TODO Print Hello World
+print("Hello World")
+
+# TODO Print the following sentence
+# Chickens can't moo
+print("Chickens can't moo")
+
+# TODO Print the following sentence
+# The cow says "Moo" (NOTICE THE QUOTATION MARKS)
+print('The cow says "Moo"')
+
+"""
+Variables
+"""
+
+# TODO Print 'the sky' using the variables whats and up
+whats = "the "
+up = "sky"
+print(whats + up)
+
+# TODO Print 5 using the two variables
+two = "2"
+three = "3"
+print(int(two)+int(three))
+
+# TODO Print "Hello World" using the variable hello
+hello = "Goodbye" # DO NOT DELETE OR CHANGE THIS LINE
+hello = "Hello World"
+print(hello) # DO NOT DELETE OR CHANGE THIS LINE
+
+# TODO Print "My move is 0, 0" using row and col
+row = 0
+col = 0
+print("My move is " + str(row) + ", " + str(col))

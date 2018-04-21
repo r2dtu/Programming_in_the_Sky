c5b434fd064a21eb13908c5602a6d12e229a751c
gypang <gypang24@gmail.com>
2018-04-21 14:57:04 -0700

    finalized starter code

--- intro_starter_code/Starter-Exercises.py
+++ intro_starter_code/Starter-Exercises.py
@@ -1,7 +1,7 @@
 """
 File: Starter-Exercises
 Description: Part of the IEEExHKN Programming in the Sky Workshop on April
-		     22, 2018. This file simply contains a few quick warmup exercises.
+	     22, 2018. This file simply contains a few quick warmup exercises.
 Editor:	Antony Nguyen, Godwin Pang
 """
 
@@ -35,8 +35,8 @@ These exercises will cover strings
 # TODO Print the following sentence
 # Chickens can't moo
 
-# TODO Print the following sentence
-# The cow says "Moo" (NOTICE THE QUOTATION MARKS)
+# TODO Print the following sentence (NOTICE THE QUOTATION MARKS)
+# The cow says "Moo"
 
 """
 Variables
@@ -47,13 +47,44 @@ whats = "the "
 up = "sky"
 
 # TODO Print 5 using the two variables
+# Hint: Note the "s surrounding the numbers
 two = "2"
 three = "3"
 
-# TODO Print "Hello World" using the variable hello
+# TODO Print "Hello World" using the variable hello by reassigning it to another
+#      value
 hello = "Goodbye" # DO NOT DELETE OR CHANGE THIS LINE
 print(hello) # DO NOT DELETE OR CHANGE THIS LINE
 
 # TODO Print "My move is 0, 0" using row and col
 row = 1
 col = 2
+
+
+"""
+List Comprehension
+"""
+
+list1 = [0,1,2,3,4]
+list2 = [0,1,2,3,2,1,0]
+list3 = [1,1]
+
+#TODO Create a new list called newlist1 that contains all the integers from 0
+#     to 4 inclusive
+
+# Code goes directly below this line and above the if statement
+
+if(newlist1 == list1):
+    print("You passed List Comprehension Exercise 1")
+else:
+    print("You failed List Comprehension Exercise 1")
+
+#TODO Create a new list called newlist2 that contains all occurrences of 1 in
+#     list2
+
+# Code goes directly below this line and above the if statement
+
+if(newlist2 == list3):
+    print("You passed List Comprehension Exercise 2")
+else:
+    print("You failed List Comprehension Exercise 2")
